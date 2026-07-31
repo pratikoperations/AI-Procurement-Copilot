fab859606ff025a8d12039e038c81389d8ddec24
pratikoperations <pratikpatilcv@gmail.com>
2026-08-01 01:44:57 +0530

    C3.3 phase-boundary contract test

--- tests/test_c3_steel_contract.py
+++ tests/test_c3_steel_contract.py
@@ -155,9 +155,9 @@ def test_c1_and_c2_governance_records_remain_present():
     assert (ROOT / "docs" / "C2_FLEXIBLE_LAMINATES_CLOSURE.md").is_file()
 
 
-def test_c3_phase_boundaries_allow_cost_but_not_future_modules():
+def test_c3_phase_boundaries_allow_cost_and_validation_but_not_risk():
     assert (ROOT / "modules" / "steel_cost.py").is_file()
-    assert not (ROOT / "modules" / "steel_validation.py").exists()
+    assert (ROOT / "modules" / "steel_validation.py").is_file()
     assert not (ROOT / "modules" / "steel_risk.py").exists()
 
 
